43bedd4d1870a2a121a480000b83ab71d44a0b97
christynatalisa <christynatalisa@gmail.com>
2021-12-03 18:23:51 +0800

    .

--- your_city/js/sketch.js
+++ your_city/js/sketch.js
@@ -3,7 +3,7 @@ let sliderY;
 let sliderSize;
 
 let button;
-let frame;
+let frame = false;
 
 let imgs = []; // GH: array of images
 let nextImg = 0; // GH: what the next image is
@@ -19,6 +19,8 @@ function preload() {
   imgs.push(loadImage("assets/6.png"));
 
   logo = loadImage("assets/yourcity.png");
+  spotlight = loadImage("assets/spotlight.png");
+
 }
 
 function setup() {
@@ -39,40 +41,46 @@ function setup() {
   sliderSize.style("transform", "rotate(270deg)");
   sliderSize.id("sliderSize");
 
-  buttonP = createButton("petronas");
-  buttonP.position(250, 90);
-  buttonP.mousePressed(towerImgA);
+  buttonA = createButton("petronas");
+  buttonA.position(250, 90);
+  buttonA.id('petronas');
+  buttonA.mousePressed(towerImgA);
 
   buttonB = createButton("burj khalifa");
   buttonB.position(330, 90);
+  buttonB.id('burj');
   buttonB.mousePressed(towerImgB);
 
-  buttonB = createButton("gran torre");
-  buttonB.position(420, 90);
-  buttonB.mousePressed(towerImgC);
+  buttonC = createButton("gran torre");
+  buttonC.position(420, 90);
+  buttonC.id('gran');
+  buttonC.mousePressed(towerImgC);
 
-  buttonB = createButton("chrysler");
-  buttonB.position(510, 90);
-  buttonB.mousePressed(towerImgD);
+  buttonD = createButton("chrysler");
+  buttonD.position(510, 90);
+  buttonD.id('chry');
+  buttonD.mousePressed(towerImgD);
 
-  buttonB = createButton("taj mahal");
-  buttonB.position(590, 90);
-  buttonB.mousePressed(towerImgE);
+  buttonE = createButton("taj mahal");
+  buttonE.position(590, 90);
+  buttonE.id('taj');
+  buttonE.mousePressed(towerImgE);
 
-  buttonB = createButton("space needle");
-  buttonB.position(670, 90);
-  buttonB.mousePressed(towerImgF);
+  buttonF = createButton("space needle");
+  buttonF.position(670, 90);
+  buttonF.id('space');
+  buttonF.mousePressed(towerImgF);
 
-  buttonB = createButton("Save Here");
-  buttonB.position(720, 770);
-  buttonB.mousePressed(saveCity);
+  buttonG = createButton("Save Here");
+  buttonG.position(720, 770);
+  buttonG.id('save');
+  buttonG.mousePressed(saveCity);
 
-  //let frame = False;
 }
 
 
 class Building {
-  constructor(img, x, y, w, h, s) {
+  constructor(img, x, y, w, h) {
     this.width = w;
     this.height = h;
     this.img = img;
@@ -80,30 +88,30 @@ class Building {
     this.color = color(255, 0, 0);
     this.x = x;
     this.y = y;
-    this.scale = s;
-
+    this.scale = 1;
   }
 
   display() {
     // GH: added
-    image(this.img, this.x, this.y, this.width, this.height, this.scale);
+    scale(this.scale);
+    image(this.img, this.x, this.y, this.width, this.height);
   }
 
   move() {}
 }
 
 function towerImgA() {
-  let petronasBuilding = new Building(imgs[0], 50, 300, 120, 400, sliderSize.value());
+  let petronasBuilding = new Building(imgs[0], 50, 300, 120, 400);
   buildings.push(petronasBuilding);
 }
 
 function towerImgB() {
-  let burjBuilding = new Building(imgs[1], 50, 300, 200, 360, sliderSize.value());
+  let burjBuilding = new Building(imgs[1], 50, 300, 200, 360);
   buildings.push(burjBuilding);
 }
 
 function towerImgC() {
-  let granBuilding = new Building(imgs[2], 30, 200, 120, 400, sliderSize.value());
+  let granBuilding = new Building(imgs[2], 30, 200, 120, 400);
   buildings.push(granBuilding);
 }
 
@@ -122,13 +130,12 @@ function towerImgF() {
 }
 
 function framefunction() {
-  fill(White);
+  fill(0);
   //add black rects to cover all the stuffs
-  rect(0, 10, 200, 800);
-  rect(0, 10, 800, 300);
-  rect(900, 10, 300, 900);
-  rect(10, 900, 900, 300);
-
+  // rect(0, 10, 200, 800);
+  // rect(0, 10, 800, 300);
+  // rect(900, 10, 300, 900);
+  // rect(10, 900, 900, 300);
 }
 
 function saveCity() {
@@ -136,10 +143,48 @@ function saveCity() {
   //frame = True;
   //if (frame == True) {
     framefunction();
+    saveCanvas('myCanvas', 'png');
+    frame = true;
 
-  saveCanvas('myCanvas', 'png');
+    spotlight1 = image(spotlight, 300, 10, 300, 300);
+    spotlight2 = image(spotlight, 500, 10, 300, 300);
+    spotlight3 = image(spotlight, 700, 10, 300, 300);
 
-  //window.location = 'https//nyu.edu';
+
+  let petronasBtn = document.getElementById('petronas');
+  petronasBtn.style.display = 'none';
+  let burjBtn = document.getElementById('burj');
+  burjBtn.style.display = 'none';
+  let granBtn = document.getElementById('gran');
+  granBtn.style.display = 'none';
+  let chryBtn = document.getElementById('chry');
+  chryBtn.style.display = 'none';
+  let tajBtn = document.getElementById('taj');
+  tajBtn.style.display = 'none';
+  let spaceBtn = document.getElementById('space');
+  spaceBtn.style.display = 'none';
+  let saveBtn = document.getElementById('save');
+  saveBtn.style.display = 'none';
+
+  let slideX = document.getElementById('sliderX');
+  slideX.style.display = 'none';
+  let slideY = document.getElementById('sliderY');
+  slideY.style.display = 'none';
+  let slideSize = document.getElementById('sliderSize');
+  slideSize.style.display = 'none';
+
+  let hori = document.getElementById('horitext');
+  hori.style.display = 'none';
+  let vertic = document.getElementById('vertitext');
+  vertic.style.display = 'none';
+  let sizet = document.getElementById('sizetext');
+  sizet.style.display = 'none';
+  let donet = document.getElementById('donetext');
+  donet.style.display = 'none';
+
+  let logo2 = document.getElementById('logoo');
+  logo2.style.display = 'none';
+
 }
 
 
@@ -150,37 +195,51 @@ function draw() {
   noStroke();
   fill(92, 115, 120);
   rect(240, 120, 1000, 450, 10, 10);
-  tint(255, 128 + 128 * sin(millis() / 1000));
+  //tint(255, 128 + 128 * sin(millis() / 1000));
   logo1 = image(logo, 1050, 7, 180, 180);
+  logo1.id('logoo');
 tint(255);
 fill("White");
   //text("Design Your City, select builings here: ", 120, 30);
 
-  let  hori = text("Horizontal Slider:", 300, 630);
+  hori = text("Horizontal Slider:", 300, 630);
+  hori.id('horitext');
 
   push();
   translate(1280, 270);
   rotate(radians(-90));
-  let verti = text("Vertical Slider:", 0, 0);
+  verti = text("Vertical Slider:", 0, 0);
+  verti.id('vertitext');
+
   pop();
 
   push();
   translate(180, 570);
   rotate(radians(-90));
-  text("Size Slider:", 0, 0);
+  sizetext = text("Size Slider:", 0, 0);
+  sizetext.id('sizetext');
+
   pop();
 
-  text("Done designing Your City?", 680, 680);
+  donetext = text("Done designing Your City?", 680, 680);
+  donetext.id('donetext');
 
   if (buildings.length > 0) {
-    scale(sliderSize.value());
+
     let newestBuilding = buildings[buildings.length - 1];
+    // scale(sliderSize.value());
     newestBuilding.x = sliderX.value();
     newestBuilding.y = sliderY.value();
+
+    newestBuilding.scale = sliderSize.value();
   }
 
   // GH: call the draw method on all buildings
   for (let i = 0; i < buildings.length; i++) {
     buildings[i].display();
+  }
+
+  if (frame == true) {
+    framefunction();
   }
 }

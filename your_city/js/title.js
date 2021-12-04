02220e2d753c4045ebb1f664bf10877cd338a0d8
christynatalisa <christynatalisa@gmail.com>
2021-12-04 17:05:11 +0800

    .

--- your_city/js/title.js
+++ your_city/js/title.js
@@ -1,15 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+
+
 }
 
 function preload() {
   logo = loadImage("assets/yourcity.png");
+  click = loadSound('sounds/click.mp3');
+
+}
+
+function next() {
+  // click.play();
+  window.location = 'result.html'
 }
 
 function draw() {
   background(0);
 
-  tint(255, 128 + 128 * sin(millis() / 1000));
-  image(logo, 0, 0, 400, 400);
 
+
+  tint(255, 128 + 128 * sin(millis() / 1000));
+  image(logo, 50, 20, 400, 400);
+
+  buttonA = createButton("start");
+  buttonA.position(680, 650);
+  buttonA.id("petronas");
+  buttonA.mousePressed(next);
 }
